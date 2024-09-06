61ee0186829c0651cab160cb2ada40f21344ebe5
RuanCarlosSilva <143547755+ruansilva123@users.noreply.github.com>
2024-09-06 14:04:08 -0300

    Feature: iniciando banco    Criando banco para armazenar os dados dos poços

--- backend/spreadsheet/models.py
+++ backend/spreadsheet/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
 
+class Localization(models.Model):
+    state = models.CharField()
+    basin = models.CharField()
+    block = models.CharField()
+    countryside = models.CharField()
+
+    class Meta:
+        abstract = True
+
+
+class TypeOfWell(models.Model):
+    OPTIONS = [
+        ('land', 'Land'),
+        ('sea', 'Sea')
+    ]
+
+    category = models.CharField()
+    land_sea = models.CharField(
+        choices=OPTIONS
+    )
+
+    class Meta: 
+        abstract = True
+
+
+class Map(models.Model):
+    latitude_base_4C = models.CharField()
+    longitude_base_4C = models.CharField()
+    latitude_base_DD = models.CharField()
+    longitude_base_DD = models.CharField()
+
+    class Meta:
+        abstract = True
+
+
+class Dates(models.Model):
+    start = models.DateField()
+    finish = models.DateField()
+    conclusion = models.DateField()
+
+    class Meta: 
+        abstract = True
+
+
